d88d15f9c827c5ad66f46bfcdd5dd322c844115a
brunoamaia <brunoamaia94@gmail.com>
2020-05-04 10:40:05 -0300

    Navegação entre as paginas

---
+++ pages/aboutme/script.js
@@ -0,0 +1,11 @@
+function openmenu() {
+    //$("#nav").css("right:", 0 + "px");
+    event.preventDefault();
+    $('.nav').css({ 'right':'0px'});
+}
+
+function closenmenu() {
+    //$("#nav").css("right:", 0 + "px");
+    event.preventDefault();
+    $('.nav').css({ 'right':'-100%'});
+}
