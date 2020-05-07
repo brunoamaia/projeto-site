adcf7ffd1bb33514ec4c64895e78b64416e4284f
brunoamaia <brunoamaia94@gmail.com>
2020-05-07 11:47:02 -0300

    Alerta para ira para a página de teste

--- pages/projects/site/script.js
+++ pages/projects/site/script.js
@@ -9,3 +9,21 @@ function closenmenu() {
     event.preventDefault();
     $('.nav').css({ 'right':'-100%'});
 }
+
+function alerta() {
+    //var texto = ''
+    //var windowObjectReference;
+    var mensagem = window.confirm('O site a seguir está em deselvomvimento,'
+                        +' portanto pode apresentar instabilidades/erros.'
+                        +'\nTem certeza que pretende prosseguir?');
+    if (mensagem == true) {
+        //window.alert('Redirecionando ...')
+        window.open('./versions/v2/index.html', '_self');
+        //window.opener('./versions/v2/index.html', '_self');
+        //location.href = './versions/v2/index.html'
+        //location.href = './versions/v2/index.html'
+    } else {
+        window.alert('Ficaremos nesta mesma página')
+    }
+
+}
