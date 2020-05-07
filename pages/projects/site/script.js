d572f38d2d8602cd8ed52564836253286cb678c4
brunoamaia <brunoamaia94@gmail.com>
2020-05-07 12:05:25 -0300

    Update script.js

--- pages/projects/site/script.js
+++ pages/projects/site/script.js
@@ -11,7 +11,7 @@ function closenmenu() {
 }
 
 function alerta() {
-    var aviso = window.confirm('O site a seguir está em deselvomvimento,'
+    var aviso = window.confirm('O site a seguir está em desenvolvimento,'
                         +' portanto pode apresentar instabilidades/erros.'
                         +'\nTem certeza que pretende prosseguir?');
     if (aviso == true) {
