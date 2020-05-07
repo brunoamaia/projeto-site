ba87609b2bf2b2dab01941d7ae999b98b6192406
brunoamaia <brunoamaia94@gmail.com>
2020-05-07 16:47:01 -0300

    endereço da versão de teste

--- pages/projects/site/script.js
+++ pages/projects/site/script.js
@@ -15,7 +15,7 @@ function alerta() {
                         +' portanto pode apresentar instabilidades/erros.'
                         +'\nTem certeza que pretende prosseguir?');
     if (aviso == true) {
-        return window.open('./versions/v2/index.html', '_self');
+        return window.open('./versions/vt/index.html', '_self');
     } else {
         window.alert('Ficaremos nesta mesma página')
     }
