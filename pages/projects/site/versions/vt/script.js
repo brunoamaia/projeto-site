7f009e45ad6537f342d71784814ccdbddf2bacbc
brunoamaia <brunoamaia94@gmail.com>
2020-05-08 09:17:10 -0300

    menu fixo no topo

--- pages/projects/site/versions/vt/script.js
+++ pages/projects/site/versions/vt/script.js
@@ -1,3 +1,16 @@
+$(function () {
+    var nav = $('.header')
+
+    $(window).scroll(function () {
+        if($(this).scrollTop()>50) {
+            nav.addClass('menu-fixo')
+        }  else {
+            nav.removeClass('menu-fixo')
+        }
+    })
+    
+})
+
 function openmenu() {
     //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
