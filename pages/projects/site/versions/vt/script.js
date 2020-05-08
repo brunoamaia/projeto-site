ce37c4a9e01cd0fb41f9656f19726704b6392d5e
brunoamaia <brunoamaia94@gmail.com>
2020-05-08 11:52:07 -0300

    barra social no menu responsivo

--- pages/projects/site/versions/vt/script.js
+++ pages/projects/site/versions/vt/script.js
@@ -22,10 +22,12 @@ function openmenu() {
     //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
     $('.nav').css({ 'right':'0px'});
+    $('.social').css({ 'right':'0px'});
 }
 
 function closenmenu() {
     //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
     $('.nav').css({ 'right':'-100%'});
+    $('.social').css({ 'right':'0px'});
 }
