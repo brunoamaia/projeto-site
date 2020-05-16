72268ec8b1f5fc824e450635a7123d1b278b0eb1
brunoamaia <brunoamaia94@gmail.com>
2020-05-16 14:25:01 -0300

    soma funcionando

--- pages/projects/apps/calculadora/funcoes.js
+++ pages/projects/apps/calculadora/funcoes.js
@@ -1,10 +1,45 @@
-var operation = []
-var txt = ''
 var display = document.querySelector('div#display')
+var txt = ''
+var operation = []
+var values = []
+var posit_number = 0
+var posit_operation = 0
 
-function cleardisplay() {
+
+
+
+// ################################## Funções
+function soma() {
+    // Verificar se algum valor foi digitado
+    let n = txt.length
+    if (n > 0) {
+        values[posit_number] = Number(txt)
+        posit_number += 1
+        txt = ''
+        display.innerHTML = `${txt}`    // Caso tenha sido o primeiro valor digitado, limpa a tela
+
+        // Verifica se alguma operação ja foi realizada anteriormente
+        if (posit_operation > 0) {
+            let res = operation[posit_operation - 1] + values[posit_number - 1]
+            operation[posit_operation] = res
+            posit_operation += 1
+            display.innerHTML = `${res}`
+        } else if (posit_number > 1) { // Verifica se tem mais que 2 elementos. Caso tenha, soma os dois ultimos valores 
+            let res = values[posit_number - 1] + values[posit_number - 2]
+            operation[posit_operation] = res
+            posit_operation += 1
+            display.innerHTML = `${res}`
+        }
+    } else {
+        display.innerHTML = 'Insira um valor!'
+    }
+}
+
+function clearall() {
     txt = ''
-    display.innerHTML = `${txt}` 
+    display.innerHTML = `Reseted!`
+    values = []
+    posit_number = 0
 }
 
 
