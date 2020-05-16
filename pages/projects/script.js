ae1711f89171438e0c8e825c69652f078b0c0f1a
brunoamaia <brunoamaia94@gmail.com>
2020-05-16 09:58:09 -0300

    alerta

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -9,3 +9,14 @@ function closenmenu() {
     event.preventDefault();
     $('.nav').css({ 'right':'-100%'});
 }
+
+function alerta() {
+    var aviso = window.confirm('Como informado, o site a seguir está em desenvolvimento,'
+                        +' portanto pode apresentar instabilidades/erros.'
+                        +'\nTem certeza que pretende prosseguir?');
+    if (aviso == true) {
+        return window.open('./apps/calculadora/index.html', '_self');
+    } else {
+        window.alert('Ficaremos nesta mesma página')
+    }
+}
