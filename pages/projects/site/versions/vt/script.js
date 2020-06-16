2da1f7b1287d51181085f58d6c18d20783f5b7bf
brunoamaia <brunoamaia94@gmail.com>
2020-06-16 16:05:07 -0300

    Remover jQuery

--- pages/projects/site/versions/vt/script.js
+++ pages/projects/site/versions/vt/script.js
@@ -1,5 +1,8 @@
 
-//  Função para reposicionar a 
+/*let nav = document.getElementById("nav")
+let social = document.getElementById("social")*/
+
+//  Função para reposicionar o inicio da seção selecionada 
 //$(document).on('click', 'a[href^="#"]', function(event) {
 //    window.setTimeout(function() {
 //        window.scrollTo(window.scrollX, window.scrollY -40);
@@ -37,15 +40,23 @@ window.setTimeout(offsetAnchor, 100);
 }) */
 
 function openmenu() {
-    //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
+    document.getElementById("nav").style.right = "0px";
+    document.getElementById("social").style.right = "0px";
+
+    //$("#nav").css("right:", 0 + "px");
+    /*event.preventDefault();       // Uso de JQuery
     $('.nav').css({ 'right':'0px'});
-    $('.social').css({ 'right':'0px'});
+    $('.social').css({ 'right':'0px'});*/
 }
 
 function closenmenu() {
-    //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
+    nav.style.right = "-100%";
+    social.style.right = "-100%";
+
+    //$("#nav").css("right:", 0 + "px");
+    /*event.preventDefault();       // Uso de JQuery
     $('.nav').css({ 'right':'-100%'});
-    $('.social').css({ 'right':'-100%'});
+    $('.social').css({ 'right':'-100%'});*/
 }
