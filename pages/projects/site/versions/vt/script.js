2d92cf2713d7e02d07f60f8d1f3f2132f52f276d
brunoamaia <brunoamaia94@gmail.com>
2020-06-16 17:18:52 -0300

    Remover comandos do JQuery

--- pages/projects/site/versions/vt/script.js
+++ pages/projects/site/versions/vt/script.js
@@ -2,20 +2,10 @@ function openmenu() {
     event.preventDefault();
     document.getElementById("nav").style.right = "0px";
     document.getElementById("social").style.right = "0px";
-
-    //$("#nav").css("right:", 0 + "px");
-    /*event.preventDefault();       // Uso de JQuery
-    $('.nav').css({ 'right':'0px'});
-    $('.social').css({ 'right':'0px'});*/
 }
 
 function closenmenu() {
     event.preventDefault();
-    nav.style.right = "-100%";
-    social.style.right = "-100%";
-
-    //$("#nav").css("right:", 0 + "px");
-    /*event.preventDefault();       // Uso de JQuery
-    $('.nav').css({ 'right':'-100%'});
-    $('.social').css({ 'right':'-100%'});*/
+    document.getElementById("nav").style.right = "-100%";
+    document.getElementById("social").style.right = "-100%";
 }
