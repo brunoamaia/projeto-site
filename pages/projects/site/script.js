e89760e83587b29c98eafec0fc8236d20e525ca1
brunoamaia <brunoamaia94@gmail.com>
2020-07-17 22:33:25 -0300

    Acesso a página da versão de teste

--- pages/projects/site/script.js
+++ pages/projects/site/script.js
@@ -10,7 +10,18 @@ function closenmenu() {
     $('.nav').css({ 'right':'-100%'});
 }
 
-function alerta() {
+function alerta2() {
+    var aviso = window.confirm('O site a seguir está em desenvolvimento,'
+                        +' portanto pode apresentar instabilidades/erros.'
+                        +'\nTem certeza que pretende prosseguir?');
+    if (aviso == true) {
+        return window.open('./versions/v2/index.html', '_self');
+    } else {
+        window.alert('Ficaremos nesta mesma página')
+    }
+}
+
+function alertat() {
     var aviso = window.confirm('O site a seguir está em desenvolvimento,'
                         +' portanto pode apresentar instabilidades/erros.'
                         +'\nTem certeza que pretende prosseguir?');
